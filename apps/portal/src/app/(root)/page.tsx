d09d1f575b0dc5962dc6d53b28c28f2991d841e1
melwin <114172287+itzTedx@users.noreply.github.com>
2025-07-17 01:03:13 +0400

    Refactor logo rendering in root page to conditionally display company logo; improve styling with rounded corners for better UI consistency.

--- apps/portal/src/app/(root)/page.tsx
+++ apps/portal/src/app/(root)/page.tsx
@@ -29,16 +29,18 @@ export default async function Page() {
           <div className="flex w-full cursor-pointer items-center justify-between border-b pb-3">
             <CollapsibleTrigger asChild>
               <div className="flex w-full items-center gap-3">
-                <div className="flex size-8 items-center justify-center rounded-full border bg-white p-1">
-                  <Image
-                    src={company.logo!}
-                    alt={`${company.name}'s Logo`}
-                    title={`${company.name}'s Logo`}
-                    height={35}
-                    width={35}
-                    className="size-4 object-contain"
-                  />
-                </div>
+                {company.logo && (
+                  <div className="flex size-8 items-center justify-center rounded-sm border bg-white p-1">
+                    <Image
+                      src={company.logo}
+                      alt={`${company.name}'s Logo`}
+                      title={`${company.name}'s Logo`}
+                      height={35}
+                      width={35}
+                      className="size-4 object-contain"
+                    />
+                  </div>
+                )}
                 <h2 className="font-medium capitalize">{company.name}</h2>
               </div>
             </CollapsibleTrigger>
